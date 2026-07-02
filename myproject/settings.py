fbee6464e155539e7241b8fb12715e1a30363273
Chitra <chitra.n@tataelxsi.co.in>
2026-07-02 10:33:24 +0530

    S3 integration to upload images

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lynx',
-    'crispy_forms'
+    'crispy_forms',
+    'storages'
 ]
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
@@ -129,3 +131,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+
+STORAGES =  {
+    "default":{
+        "BACKEND" : "storages.backends.s3boto3.S3StaticStorage"
+    },
+    "staticfiles":{
+        "BACKEND" : "storages.backends.s3boto3.S3StaticStorage"
+    },
+}
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
